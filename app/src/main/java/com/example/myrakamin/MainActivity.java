ad7388b8e949d225d5dfca134de00d1f3750be55
Ikrar Bagaskara <ikrarb95@gmail.com>
2023-10-07 09:03:54 +0700

    add firebase auth

--- app/src/main/java/com/example/myrakamin/MainActivity.java
+++ app/src/main/java/com/example/myrakamin/MainActivity.java
@@ -1,23 +1,14 @@
 package com.example.myrakamin;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class wMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Create an Intent to start HomeActivity
-        Intent intent = new Intent(this, HomeActivity.class);
-
-        // Start HomeActivity
-        startActivity(intent);
-
-        // Finish MainActivity if you want it removed from the back stack
-        finish();
     }
 }
