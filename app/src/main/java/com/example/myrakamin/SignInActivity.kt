9397d03d2eaf6582aa3ec5670d44483ac47b593e
Ikrar Bagaskara <ikrarb95@gmail.com>
2023-10-10 15:47:47 +0700

    ADD: test and example for navigation

--- app/src/main/java/com/example/myrakamin/SignInActivity.kt
+++ app/src/main/java/com/example/myrakamin/SignInActivity.kt
@@ -33,7 +33,7 @@ class SignInActivity : AppCompatActivity() {
 
                 firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
                     if (it.isSuccessful) {
-                        val intent = Intent(this, MainActivity::class.java)
+                        val intent = Intent(this, CekSaldoActivity::class.java)
                         startActivity(intent)
                     } else {
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
